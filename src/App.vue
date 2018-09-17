2ff397290bee4d4989562dd1b68eb10dfb1cb7f1
Benjamin Graf <benjamin.graf@z-lab.com>
2018-09-17 21:43:11 +0200

    Initial productlist/basket spa

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container my-3 p-3">
+    <h2>Products:</h2>
+    <ul>
+      <li v-for="product in products" class="m-2">
+        <strong>
+          {{ product.name }}: {{ product.price }}€
+        </strong>
+        <button class="btn btn-sm btn-primary"
+                v-on:click="addProductToBasket(product)">
+          Add to basket
+        </button>
+      </li>
+    </ul>
+    <basket v-bind:products="productsInBasket" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Basket from './components/Basket.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: { Basket },
+  data: function() {
+    return {
+      products: [
+        { name: 'Schaufel', price: 50 },
+        { name: 'Fork', price: 70 }
+      ],
+      productsInBasket: []
+    }
+  },
+  methods: {
+  	addProductToBasket: function(product) {
+    	this.productsInBasket.push(product);
+    },
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #333;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #fff;
+  border-radius: 8px;
 }
 </style>
---
+++ src/components/Basket.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>Basket:</h2>
+    <ul v-if="products.length !== 0">
+      <li v-for="product in products" class="m-2">
+        {{ product.name }}@{{ product.price }}€
+        <button class="btn btn-sm btn-danger"
+                v-on:click="removeProduct(product)">
+          Remove
+        </button>
+      </li>
+      <li>
+        <strong>Sum: {{ basketSum }}€</strong>
+      </li>
+    </ul>
+    <p v-else>
+      Your basket is still empty. Why don't you add something?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'basket',
+  props: [
+  	'products'
+  ],
+  data: function() {
+		return { }
+  },
+  computed: {
+  	basketSum: function() {
+    	return this.products.reduce((sum, product) => sum + product.price, 0);
+    }
+  },
+  methods: {
+  	removeProduct: function (product) {
+    	this.products.splice(this.products.indexOf(product), 1);
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
